721e7e5c85ced50940cf90af335db7a802fff96f
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2022-02-07 07:45:42 +0200

    fixed bugs

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -4,22 +4,9 @@ import { ContactForm, ContactLabel, TelLabel, InputName } from "./Form.styled"
 
 class Form extends Component {
     state = {
-        contacts: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ],
-        filter: '',
         name: '',
         number: ''
     }
-    // state = {
-    //     contacts: [],
-    //     filter: '',
-    //     name: '',
-    //     number: ''
-    // };
 
     nameInputId = nanoid();
 
@@ -45,7 +32,6 @@ class Form extends Component {
 
     reset = () => {
         this.setState({
-            contacts: [],
             name: '',
             number: ''
         })
